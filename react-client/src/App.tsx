2e5a369770fe81d1fad0c19d5e1bfe7ebb219dde
vostavhy <vostavhy@gmail.com>
2025-02-15 16:35:10 +0100

    initial commint: vite redux toolkit daisy-ui

---
+++ react-client/src/App.tsx
@@ -0,0 +1,12 @@
+const App = () => {
+  return (
+    <div className="App">
+      <header className="">
+        <h1 className="text-6xl">React App</h1>
+        <button className="btn btn-primary rounded-2xl">Click on me</button>
+      </header>
+    </div>
+  )
+}
+
+export default App
---
+++ react-client/vite.config.ts
@@ -0,0 +1,17 @@
+import { defineConfig } from "vitest/config"
+import react from "@vitejs/plugin-react"
+import tailwindcss from "@tailwindcss/vite"
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react(), tailwindcss()],
+  server: {
+    open: true,
+  },
+  test: {
+    globals: true,
+    environment: "jsdom",
+    setupFiles: "src/setupTests",
+    mockReset: true,
+  },
+})
